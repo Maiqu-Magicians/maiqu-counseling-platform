c26bc0759f11c396d5eab9a0e19d414da08e8269
Lin <1725525653@qq.com>
2022-07-12 11:15:37 +0800

    init the project

---
+++ src/main/java/tech/maiquer/maiqucounseling/MaiquCounselingApplication.java
@@ -0,0 +1,13 @@
+package tech.maiquer.maiqucounseling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaiquCounselingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaiquCounselingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/maiquer/maiqucounseling/MaiquCounselingApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.maiquer.maiqucounseling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaiquCounselingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
